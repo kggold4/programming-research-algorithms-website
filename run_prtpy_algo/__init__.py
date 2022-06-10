bfa72b925100fea32d729e1d5f6607dee975990b
kggold4 <68167734+kggold4@users.noreply.github.com>
2022-06-10 22:03:53 +0300

    add import to run main file

--- run_prtpy_algo/__init__.py
+++ run_prtpy_algo/__init__.py
@@ -4,7 +4,7 @@ import sys
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = 'ecf6e975838a2f7bf3c5dbe7d55ebe5b'
 sys.path.append('/home/kggold4/prtpy')
 
 from run_prtpy_algo import routes
