e9ef68d539af99dfc70507f2feb8d881d9713851
kggold4 <68167734+kggold4@users.noreply.github.com>
2022-06-10 22:00:38 +0300

    add import to run main file

--- run_prtpy_algo/__init__.py
+++ run_prtpy_algo/__init__.py
@@ -5,10 +5,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-sys.path.append('../prtpy')
+sys.path.append('/home/kggold4/prtpy')
 
-from prtpy.partitioning.kk import kk
-from prtpy.partitioning.ckk import ckk
-from prtpy.partitioning.rnp import rnp
-from prtpy.partitioning.irnp import irnp
 from run_prtpy_algo import routes
